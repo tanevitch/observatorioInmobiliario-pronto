1d60f77e72d95a13b6af2e1d75773beeaf185e31
tanevitch <lubatane@gmail.com>
2025-02-10 03:05:25 +0000

    cambio en el mapper

--- csv2pronto/__main__.py
+++ csv2pronto/__main__.py
@@ -11,19 +11,17 @@ from joblib import Parallel, delayed
 
 def main() -> None:
     args: argparse.Namespace = parse_args()
-
+    
     with open(args.source, "r", encoding="utf-8") as csv_file:
         graph: rdflib.Graph = rdflib.Graph()
 
         graph.parse(args.ontology)
-
-        total_rows = sum(1 for _ in csv.reader(csv_file))
-        csv_file.seek(0)
-        
+       
         chunksize = 3000
 
-        Parallel(n_jobs=-1, backend='multiprocessing')(delayed(create_graph_from_chunk)(row, graph, idx, args.destination, args.format) for row, idx, _ in zip(pd.read_csv(csv_file, chunksize=chunksize, iterator=True, dialect='excel', keep_default_na=False, dtype=str), list(range(total_rows//chunksize)), tqdm(range(total_rows//chunksize))))
-
+        for idx, row in enumerate(pd.read_csv(csv_file, chunksize=chunksize, iterator=True, dialect='excel', delimiter=",", keep_default_na=False, dtype=str)):
+            # Process each chunk sequentially
+            create_graph_from_chunk(row, graph, idx, args.destination, args.format)
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description=__doc__)
